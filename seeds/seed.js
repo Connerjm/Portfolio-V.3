ae0b97f53145835e4c9c4228356e9f0872aa6e0f
Conner Martin <connerjm2@gmail.com>
2021-04-11 22:16:15 -0700

    Added the faker package to create some fake user data.

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,16 +1,32 @@
 //Imports
 const sequelize = require("../config/connection");
+const faker = require("faker");
 const { User } = require("../models");
-
-const userData = require("./userData.json");
 
 //Functino to create test data.
 async function seedData()
 {
+    const userAmount = 10;//Create 10 fake users.
+    const users = [];//An array to hold the created user objects.
+
+    for(let x = 0; x < userAmount; x++)
+    {
+        let user =
+        {
+            username: faker.internet.userName(),
+            email: faker.internet.email(),
+            password: faker.internet.password()
+        }
+        users.push(user);
+    }
+
+    //Recreate the database.
     await sequelize.sync({ force: true });
 
-    await User.bulkCreate(userData, { individualHooks: true });
+    //Bulk create all the user entries.
+    await User.bulkCreate(users, { individualHooks: true });
 
+    //Exit when complete.
     process.exit(0);
 }
 

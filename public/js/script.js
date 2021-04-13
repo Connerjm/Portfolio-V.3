a0ca9c4a61dc675afe6adbe475b80091b3752e95
Conner Martin <connerjm2@gmail.com>
2021-04-12 21:09:10 -0700

    switched to bulma styling cause css is hard lul

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,10 @@
 $(document).ready(() =>
 {
-    //Show or hide the menu links.
-    $("#nav-button").click(() =>
-    {
-        $("#links").toggle();
-    });
+    // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(() =>
+  {
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+  });
 });

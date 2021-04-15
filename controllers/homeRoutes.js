bdad14d64290bad891f38aed745134437aa75471
Conner Martin <connerjm2@gmail.com>
2021-04-14 19:52:37 -0700

    Moved test html to main handlebars file and added all the pages routes.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,7 +4,37 @@ const router = require("express").Router();
 //Home.
 router.get("/", (req, res) =>
 {
-    res.status(200).json({ message: "Hello, World!" });
+    res.render("home");
+});
+
+//Portfolio.
+router.get("/portfolio", (req, res) =>
+{
+    res.render("portfolio");
+});
+
+//Contact.
+router.get("/contact", (req, res) =>
+{
+    res.render("contact");
+});
+
+//Login.
+router.get("/login", (req, res) =>
+{
+    //TODO
+});
+
+//Sign up.
+router.get("/signup", (req, res) =>
+{
+    //TODO
+});
+
+//Everything else / 404 page someday.
+router.get("*", (req, res) =>
+{
+    res.redirect("/");
 });
 
 //Exports.
